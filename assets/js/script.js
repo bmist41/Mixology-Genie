1c601e8978602f02bc78fe0a1a558f0908f4aca8
Hunter Thompson <hunterthompson025@gmail.com>
2024-05-09 21:16:27 -0700

    this commit adds the image to the DOM and notes on the script file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,10 +19,13 @@ try {
 	console.log(result);
 
   const drinkTitleEl = document.createElement('h2');
+  const drinkImageEl = document.createElement('img');
 
   drinkTitleEl.textContent = result[0].title;
-  
+  drinkImageEl.src = `https://apipics.s3.amazonaws.com/coctails_api/${result[0].id}.jpg`;
+
   containerEl.append(drinkTitleEl);
+  containerEl.append(drinkImageEl);
 
 } catch (error) {
 	console.error(error);
@@ -31,17 +34,18 @@ getRandomDrink();
 //function renderResult
 
 //pass the id through the apiURL
-async function renderDrinkId (id) {
-  try {
-    const response = await fetch(`https://the-cocktail-db3.p.rapidapi.com/1`, options);
-    const result = await response.text();
-    console.log(result);
-    //response = result;
-    //invoke function to render result
-  } catch (error) {
-    console.error(error);
-  }}
-renderDrinkId();
+//if extra time we can add the recipe from a second api call on the cocktail db api
+// async function renderDrinkId (id) {
+//   try {
+//     const response = await fetch(`https://the-cocktail-db3.p.rapidapi.com/1`, options);
+//     const result = await response.text();
+//     console.log(result);
+//     //response = result;
+//     //invoke function to render result
+//   } catch (error) {
+//     console.error(error);
+//   }}
+// renderDrinkId();
 
 //activate modal
 
