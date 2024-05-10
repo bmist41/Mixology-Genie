02885b3ebe4ab466a8caa9544aa41ab76b7d6d26
Bear M <bear.mistele@gmail.com>
2024-05-09 18:54:07 -0700

    Update script.js

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems);
-  });
-
-  // Or with jQuery
